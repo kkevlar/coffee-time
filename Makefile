f7b92ec046a90956b2e71e3d414a89935651c7b9
Kevin Kellar <kellar.kevin@gmail.com>
2019-01-13 23:58:42 -0800

    a safe coffee in the morning

--- Makefile
+++ Makefile
@@ -12,7 +12,7 @@ ino/Arduino-Makefile: | ino
 	cd ino && git clone git@github.com:sudar/Arduino-Makefile.git
 
 ino/coffee.cpp: coffee.cpp | ino
-	python3 timetill9am.py | sed -r 's;(.*);cat coffee.cpp | sed -r "s:FUTURE_SECONDS:\1*1000:" > ino/coffee.cpp;' | bash
+	python3 timetill9am.py | sed -r 's;(.*);cat coffee.cpp | sed -r "s:FUTURE_SECONDS:\1:" > ino/coffee.cpp;' | bash
 
 ino/coffee.hpp: coffee.hpp | ino
 	cp coffee.hpp ino
--- coffee.cpp
+++ coffee.cpp
@@ -5,11 +5,15 @@ Servo s;
 void boil()
 {
 	s.write(180);
+	delay(2000);
+	s.write(90);
 }
 
 void noboil()
 {
 	s.write(0);
+	delay(2000);
+	s.write(90);
 }
 
 void coffee()
@@ -20,9 +24,13 @@ void coffee()
 	unsigned long duration_off_init;
 	unsigned long duration_off_again;
 
-	duration_on = 1000*3600;
-	duration_off_again = 23*3600*1000;
+	duration_on = 1000;
+	duration_off_again = 23*1000;
 	duration_off_init = FUTURE_SECONDS;
+
+	duration_on *= 1000;
+	duration_off_init *= 1000;
+	duration_off_again *= 3600;
 
 	earlier_time = millis();
 	while(1)
@@ -45,6 +53,14 @@ void coffee()
 				earlier_time = millis();
 				noboil();
 				break;
+			}
+			else
+			{
+				boil();
+				for(int z = 0; z < 60; z++)
+				{
+				delay(5 * 1000);
+				}
 			}
 		}
 		while(1)
@@ -70,14 +86,21 @@ void setup()
 void loop() 
 {
 	s.attach(3);
-	delay(2000);
+	delay(3000);
 	noboil();
-	delay(2000);
+	delay(3000);
 	boil();
-	delay(2000);
+	delay(3000);
 	noboil();
-	delay(2000);
-
+	delay(3000);
+	boil();
+	delay(3000);
+	noboil();
+	delay(3000);
+	boil();
+	delay(3000);
+	noboil();
+	delay(3000);
 
 	coffee();
 }
